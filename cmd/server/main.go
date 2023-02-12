7b61887c2e608b7a3913c2b32e59ed819f96152b
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2023-02-12 12:21:59 +0100

    Added new base URL

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,11 +17,11 @@ import (
 	"go.uber.org/zap"
 )
 
-// @title			Candles API
-// @description	This is API specification for Candels, a OHLC data API platform.
-// @version		1.0
-// @host			localhost:8080
-// @BasePath		/
+//	@title			Candles API
+//	@description	This is API specification for Candels, a OHLC data API platform.
+//	@version		1.0
+//	@host			https://candles.onrender.com
+//	@BasePath		/
 func main() {
 	//init dependencies
 	conf := config.Init()
